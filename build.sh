35170537d0e0d412673f2456d3117ed929b76c38
loynoir <loynoir@users.noreply.github.com>
2025-08-09 13:33:34 +0000

    add miri

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@ set -euo pipefail
 
 mkdir -p ./target ./build
 
+rustup component add --toolchain nightly-2025-08-06-x86_64-unknown-linux-gnu miri
+
+# note: workaround miri tmp exe
+# could not execute process `/tmp/.tmpXXX/target/custom_sysroot/build/std-XXX/build-script-build`
+# sudo mount -o remount exec /tmp
+
+cargo miri setup
+cargo miri test
+
 cargo build
 cargo build -r
 
