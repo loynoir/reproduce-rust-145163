6ed2ea4b20fb08ccbc41db64a7dd14f3dcce72ee
loynoir <loynoir@users.noreply.github.com>
2025-08-09 13:40:16 +0000

    add miri

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir -p ./target ./build
 
 rustup component add --toolchain nightly-2025-08-06-x86_64-unknown-linux-gnu miri
 
-# note: workaround miri tmp exe
+# note: workaround miri tmp exe within scratch devcontainer
 # could not execute process `/tmp/.tmpXXX/target/custom_sysroot/build/std-XXX/build-script-build`
 # sudo mount -o remount exec /tmp
 
